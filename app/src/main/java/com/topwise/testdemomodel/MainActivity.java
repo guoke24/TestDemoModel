679baad48b75518101ae28aab3a1e234256cff4b
guohao <guohao@topwisesz.com>
2019-04-05 16:53:41 +0800

    first commit

---
+++ app/src/main/java/com/topwise/testdemomodel/MainActivity.java
@@ -0,0 +1,19 @@
+package com.topwise.testdemomodel;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends BaseTestActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setContentView(R.layout.activity_main);
+        super.onCreate(savedInstanceState);
+    }
+
+    public void test_1(View v){
+        //do something
+        showMessage("click test_1 bt");
+    }
+
+}
